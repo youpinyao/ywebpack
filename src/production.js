0704e624901967bbeaed4c84341d7b2d511d3749
游品尧 <497400448@qq.com>
2018-05-29 17:18:29 +0800

    vendor 可配置

--- src/production.js
+++ src/production.js
@@ -3,44 +3,47 @@ const webpackMerge = require('webpack-merge');
 const baseConfig = require('./util/base');
 
 module.exports = function (config) {
-  return webpackMerge(baseConfig(config), config.webpackMerge || {}, {
-    plugins: [
-      new webpack.LoaderOptionsPlugin({
-        minimize: false,
-        debug: false
-      }),
+  const plugins = [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
 
-      // 输出公共模块
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      beautify: false,
+      comments: false,
+      mangle: {
+        screw_ie8: false,
+        keep_fnames: false
+      },
+      compress: {
+        screw_ie8: false,
+        warnings: false
+      },
+      output: {
+        screw_ie8: false
+      }
+    })
+  ];
+
+  if (config.vendors !== false && config.vendor !== false) {
+    plugins.push( // 输出公共模块
       new webpack.optimize.CommonsChunkPlugin({
         name: ['vendor'],
         minChunks(module) {
           // this assumes your vendor imports exist in the node_modules directory
           return module.context && module.context.indexOf('node_modules') !== -1;
         }
-      }),
+      }))
+  }
 
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production')
-        }
-      }),
-
-      new webpack.optimize.UglifyJsPlugin({
-        sourceMap: false,
-        beautify: false,
-        comments: false,
-        mangle: {
-          screw_ie8: false,
-          keep_fnames: false
-        },
-        compress: {
-          screw_ie8: false,
-          warnings: false
-        },
-        output: {
-          screw_ie8: false
-        }
-      })
-    ]
+  return webpackMerge(baseConfig(config), config.webpackMerge || {}, {
+    plugins,
   });
 };

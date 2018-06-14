5e76f6c0e8a535e8f044521bb4b88625ea9876da
游品尧 <497400448@qq.com>
2018-06-14 17:41:50 +0800

    1

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 
 import $ from 'jquery';
 import moment from 'moment';
+import tpl from './tpl.html';
 import less from './index.less';
 import scss from './index.scss';
 import css from './index.css';
@@ -11,3 +12,4 @@ console.log('css', JSON.stringify(css), css);
 console.log('Promise', Promise);
 console.log('jquery', $);
 console.log('moment', moment);
+console.log('tpl', tpl);
